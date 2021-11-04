beba91a369bf60615d1c030e4dc8268d95500b1b
SAHCOZY <58663587+SAH-COZY@users.noreply.github.com>
2021-11-04 15:37:47 +0100

    fix exploitable events

--- Walker_Bank/sv_walker.lua
+++ Walker_Bank/sv_walker.lua
@@ -7,38 +7,32 @@ AddEventHandler("Walker_Bank:deposerargent", function(amount)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
     local xMoney = xPlayer.getMoney()
-    print(xPlayer.getMoney()) -- j'ai eu la flemme de le supp quand j'étais entrain de le faire hahah (c'est walker)
-    if amount == 0  then
-        return
-    end
-    if xMoney == 0 then
-        TriggerClientEvent('esx:showNotification', source, "~r~Vous n'avez pas assez ~r~d\'argent~s~ !")
-    else
-    xPlayer.addAccountMoney('bank', amount)
-    xPlayer.removeMoney(amount)
+
+    if xMoney >= amount then
+	xPlayer.removeMoney(amount)
+	xPlayer.addAccountMoney('bank', amount)
  
-         TriggerClientEvent('esx:showNotification', source, "~g~Vous avez deposé ~g~"..amount.."$~s~ à la banque !")
+	TriggerClientEvent('esx:showNotification', source, "~g~Vous avez deposé ~g~"..amount.."$~s~ à la banque !")
+    else
+        TriggerClientEvent('esx:showNotification', source, "~r~Vous n'avez pas assez ~r~d\'argent~s~ !")
     end    
 end)
+
 RegisterServerEvent("Walker_Bank:retirerargent")
 AddEventHandler("Walker_Bank:retirerargent", function(amount)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
     local bankmoney = xPlayer.getAccount('bank').money
-    print(xPlayer.getMoney()) -- celui la aussi faites le si vous voulez le supp allez y haha
 
-    if amount == 0 then
-        return
-    end
-    if bankmoney == 0 then
-        TriggerClientEvent('esx:showNotification', source, "~r~Vous n'avez pas assez d'argent sur votre compte bancaire !")
-    else
-        xPlayer.removeAccountMoney('bank', amount)
+    if bankmoney >= amount then
+	xPlayer.removeAccountMoney('bank', amount)
         xPlayer.addMoney(amount)
-     
-             TriggerClientEvent('esx:showNotification', source, "~g~Vous avez retiré ~g~"..amount.."$~s~ de la banque !")
+        TriggerClientEvent('esx:showNotification', source, "~g~Vous avez retiré ~g~"..amount.."$~s~ de la banque !")
+    else
+        TriggerClientEvent('esx:showNotification', source, "~r~Vous n'avez pas assez d'argent sur votre compte bancaire !")  
     end
 end)
+
 RegisterServerEvent("walker:solde") 
 AddEventHandler("walker:solde", function(action, amount)
     local _source = source
@@ -47,30 +41,24 @@ AddEventHandler("walker:solde", function(action, amount)
 
     TriggerClientEvent("walker:solderefresh", source, playerMoney)
 end)
+
 RegisterServerEvent('Walker_Bank:virement')
 AddEventHandler('Walker_Bank:virement', function(receiver, amount)
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	local xPlayer2 = ESX.GetPlayerFromId(receiver)
-	local virement = 0
-    local bankmoneysender = xPlayer.getAccount('bank').money
+    local _src = source
+    local xPlayer = ESX.GetPlayerFromId(_src)
+    local xTarget = ESX.GetPlayerFromId(receiver)
+    local Sender_BankMoney = xPlayer.getAccount('bank').money
 
-    if xPlayer2 == nil or xPlayer2 == -1 or source == tonumber(receiver) then
-        TriggerClientEvent('esx:showNotification', source, "~r~Ce destinataire n'éxiste pas sur notre base de données")
-        
+    if xTarget == nil or xTarget == -1 or _src == tonumber(receiver) then
+        TriggerClientEvent('esx:showNotification', source, "~r~Ce destinataire n'existe pas sur notre base de données")
     else
-        if bankmoneysender == 0 then
-
-            TriggerClientEvent('esx:showNotification', source, "~r~Vous n'avez pas assez d'argent sur votre compte bancaire")
-        else
-            print(bankmoneysender)
+	if Sender_BankMoney >= amount then
             xPlayer.removeAccountMoney('bank', tonumber(amount))
-            xPlayer2.addAccountMoney('bank', tonumber(amount))
-            TriggerClientEvent('esx:showNotification', source, "~g~Transfert réussi. Vous avez envoyé "..tonumber(amount).." $ à "..xPlayer2.getName())
+            xTarget.addAccountMoney('bank', tonumber(amount))
+            TriggerClientEvent('esx:showNotification', source, "~g~Transfert réussi. Vous avez envoyé "..tonumber(amount).." $ à "..xTarget.getName())
             TriggerClientEvent('esx:showNotification', receiver, "~g~Vous avez reçu un virement de "..tonumber(amount).." $ de la part de "..xPlayer.getName())
-
-        end
+	else
+	    TriggerClientEvent('esx:showNotification', source, "~r~Vous n'avez pas assez d'argent sur votre compte bancaire")
+	end
     end
-
-
 end)
